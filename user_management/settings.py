82ac797e7dda92b9673a994cfc45bcb2cc045322
Ubuntu <ubuntu@ip-172-31-87-99.ec2.internal>
2023-01-23 14:30:51 +0000

    Addded Dockerfile

--- user_management/settings.py
+++ user_management/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['44.211.144.153','localhost']
 
 
 # Application definition
