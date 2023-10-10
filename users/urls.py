d0cf223a1bb010d57cae1c5fed0bb19ffee31443
johnnalwa <108821007+johnnalwa@users.noreply.github.com>
2023-10-10 04:17:00 +0300

    new

--- users/urls.py
+++ users/urls.py
@@ -9,18 +9,14 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='users-register'),
     path('profile/', profile, name='users-profile'),
     path('add_client/', views.add_client, name='add_client'),
-    path('success/', views.success_page, name='success_page'), 
+    # path('success/', views.success_page, name='success_page'), 
     
     path('calendar/', views.calendar_view, name='calendar_view'),
     path('attendance/', views.record_attendance, name='record_attendance'),
     
-<<<<<<< HEAD
-    path('user-clients/', views.user_clients, name='user_clients'),
-    path('admin-clients/', views.admin_clients, name='admin_clients'),
-    path('client/<int:pk>/', views.client_details, name='client_details'),
-=======
-    
->>>>>>> aa4d792203ac759ad6ca019f3e8a91c43bfbbcf9
+    # path('user-clients/', views.user_clients, name='user_clients'),
+    # path('admin-clients/', views.admin_clients, name='admin_clients'),
+    # path('client/<int:pk>/', views.client_details, name='client_details'),
 
   
 ]
